c1cb07b7a2c759788d7ad05d79c8635ae31c8cb5
Muhammad Shariq <shariqmuhammad@yahoo.com>
2013-04-03 21:21:43 +0300

    Finish Movie App

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 MovieApp::Application.routes.draw do
+  resources :comments
+
+
+  resources :movies
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
